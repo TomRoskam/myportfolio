906c752f6def5c1f3d49e139bfc36dcd328bd082
TomRoskam <tomroskam38@gmail.com>
2019-06-04 14:04:59 +0200

    Change background image

--- resources/views/interests.blade.php
+++ resources/views/interests.blade.php
@@ -21,7 +21,7 @@
                 <img class="interestsImage" src="images/porsche.jpg">
             </div>
             <div class="cardText">
-                <p>Porsche 911s</p>
+                <p>Porsche 911s is the fastest original Porsche. A nimble car which is know for its perfect steering feel.</p>
             </div>
         </div>
 
@@ -30,7 +30,7 @@
                 <img class="interestsImage" src="images/ford_mustang.jpg">
             </div>
             <div class="cardText">
-                <p>Ford Mustang Fastback 1967</p>
+                <p>The Ford Mustang Fastback 1967 is the most beautiful Mustang ever build. Powered by a V8 it delivers a fantastic sound.</p>
             </div>
         </div>
 
@@ -48,7 +48,7 @@
                 <img class="interestsImage" src="images/bronco.jpg">
             </div>
             <div class="cardText">
-                <p>Ford Bronco</p>
+                <p>The Ford Bronco is a powerful off road machine. This machine will get you through mud or deserts.</p>
             </div>
         </div>
 
@@ -57,7 +57,7 @@
                 <img class="interestsImage" src="images/drum.jpg">
             </div>
             <div class="cardText">
-                <p>I have played drums for about 9 years.</p>
+                <p>I have played drums for about 9 years. I still really enjoy playing drums, I should spend more time playing.</p>
             </div>
         </div>
 
@@ -66,7 +66,7 @@
                 <img class="interestsImage" src="images/tech.jpg">
             </div>
             <div class="cardText">
-                <p>I always liked technology.</p>
+                <p>I always liked technology. I like phones and laptops when they look really clean and be very powerful.</p>
             </div>
         </div>
     </div>
